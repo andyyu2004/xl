cd4aad0818ad4cafe29579be0450bfbd734932af
Andy Yu <andyyu2004@gmail.com>
2022-01-15 20:21:23 +1300

    init

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Spreadsheet from "./lib/Spreadsheet.svelte";
+</script>
+
+<Spreadsheet />
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
---
+++ src/lib/Spreadsheet.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  interface Grid {
+    cells: Cell[][];
+  }
+
+  interface Cell {
+    content: Content;
+  }
+
+  type Content = string | number;
+  const grid: Grid = {
+    cells: [
+      [{ content: "one" }, { content: "two" }],
+      [{ content: "three" }, { content: "four" }],
+      [{ content: "five" }, { content: "six" }],
+    ],
+  };
+</script>
+
+<main>
+  <table>
+    {#each ["", "a", "b", "c", "d", "e", "f"] as header}
+      <th>{header}</th>
+    {/each}
+    {#each grid.cells as row, i}<td />
+      <tr>
+        <th>{i}</th>
+        {#each row as col}
+          <td>{col.content}</td>
+        {/each}
+      </tr>
+    {/each}
+  </table>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
